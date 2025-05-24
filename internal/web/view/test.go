450da88bce781636a667b4df28283e499cd0020f
Patryk Magoń (laptop) <magonskyy@gmail.com>
2025-05-24 21:14:58 +0200

    example templating

--- internal/web/view/test.go
+++ internal/web/view/test.go
@@ -1,15 +1,31 @@
 package view
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Dependencies struct {
 }
 
 func RegisterRoutes(router *gin.Engine, deps Dependencies) {
-	// Define your routes here
+	router.SetFuncMap(template.FuncMap{
+		"title": func(s string) string {
+			return strings.ToUpper(s)
+		},
+	})
+	router.LoadHTMLGlob("internal/web/view/templates/*")
+
 	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Test route is working",
-		})
+		data := gin.H{
+			"Message":     "This is a dynamic message!",
+			"ShowDetails": true,
+			"Items":       []string{"Item 1", "Item 2", "Item 3"},
+			"Name":        "john doe",
+		}
+		c.HTML(http.StatusOK, "test.html", data)
 	})
 }
